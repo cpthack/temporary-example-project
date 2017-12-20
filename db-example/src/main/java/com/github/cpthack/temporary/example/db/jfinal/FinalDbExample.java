869cf535e157cb80c639f9aea5f9e746c8c03e3d
cpthack <yu.zhou@codrim.net>
2017-12-20 18:56:56 +0800

    调整jfinal的db示例的包名.

--- db-example/src/main/java/com/github/cpthack/temporary/example/db/jfinal/FinalDbExample.java
+++ db-example/src/main/java/com/github/cpthack/temporary/example/db/jfinal/FinalDbExample.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.cpthack.temporary.example.db;
+package com.github.cpthack.temporary.example.db.jfinal;
 
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.activerecord.Db;
